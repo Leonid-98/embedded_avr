ee11643a2619cbb32109ca1ed96132fdc69d09c9
Leonid-98 <leonid.tsigrinski@gmail.com>
2022-03-28 13:33:20 +0300

    123

--- praks5/koduto.asm
+++ praks5/koduto.asm
@@ -19,34 +19,45 @@ sts  ADCSRA, r16
 ;LED INIT
 ldi r16, 0xFF
 out DDRA, r16
-ldi r16, 1<<PA0
+ldi r16, (1<<PA0)
 out PORTA, r16
 
 .equ msb_mask = 0b10 // 10th bit in 16bit val
-.equ xor_mask = 1 // 10 bit is only required
+.equ xor_mask = 0b01 // 9th bit is only required
+.equ stop_mask_h = 0b01 
+.equ stop_mask_l = 0b1111_0000
 
 main:
+    // read adc value
 	wait_for_adc:
 		ldi r16, ADCSRA
-		// This bit is set when an ADC conversion completes
 		andi r16, (1<<ADIF) 
 		breq wait_for_adc
-	lds r0, ADCL
-	lds r1, ADCH
-	;out PORTA, r1
+	lds r20, ADCL
+	lds r21, ADCH
 
-	// check whis bit is MSB
-	mov r16, r1
+	// check if in center (496 kuni 511 | 01 1111 xxxx)
+	// kui ADC == 01_1111_xxxx, hypab tagasi
+	mov r16, r20
+	mov r17, r21
+	andi r17, stop_mask_h
+	cpi r17, stop_mask_h
+		breq check_low_byte
+		rjmp check_passed // no stop (arv ei ole vahemikus 486 kuni 511)
+		check_low_byte:
+			andi r16, stop_mask_l
+			cpi r16, stop_mask_l
+			breq main
+	check_passed:
+
+	// check whitch bit is MSB, detects side
+	mov r16, r21
 	andi r16, msb_mask // MSB is 10th bit
 	cpi r16, msb_mask
 		breq left_side  // if msb = 1
 		rjmp right_side // else
 
 	left_side:
-		call shift_led_left
-		mov r20, r0
-		mov r21, r1
-
 		// invert 9-0 bit
 		ldi r16, 0xFF
 		eor r20, r16
@@ -54,19 +65,20 @@ main:
 		eor r21, r16
 		andi r21, xor_mask
 
+		call shift_led_left
 		rjmp continue_main
 
 	right_side:
 		call shift_led_right
-		mov r20, r0
-		mov r21, r1
-		/*ldi r20, low(200)
-		ldi r21, high(200)*/
 		rjmp continue_main
 
 	continue_main:
 
-	// delay
+	// delay + 10ms
+	ldi r16, 10
+	ldi r17, 0
+	add r20, r16
+	adc r21, r17
 	delay_ms:
 		call delay_1ms
 		subi r20, 1
@@ -77,8 +89,6 @@ main:
 
 shift_led_left:
 	push r16
-	push r20
-	push r21
 	in r16, PORTA
 
 	lsl r16
@@ -88,16 +98,11 @@ shift_led_left:
 	continue_shift:
 	out PORTA, r16
 
-	pop r21
-	pop r20
 	pop r16
 	ret
 
 shift_led_right:
 	push r16
-	push r20
-	push r21
-
 	in r16, PORTA
 
 	lsr r16
@@ -107,8 +112,6 @@ shift_led_right:
 	continue_shift2:
 	out PORTA, r16
 
-	pop r21
-	pop r20
 	pop r16
 	ret
 
@@ -120,7 +123,7 @@ delay_1ms:
 	ldi r16, 0
 	out TCNT0, r16 // TCNT0 null
 	ldi r16, 1
-	out OCR0B, r16 // compare wih 1 (NB prescaler is 1024)
+	out OCR0B, r16 // compare wih 1
 
 	loop:
 		in r16, TIFR0
---
+++ test.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+# root window
+root = tk.Tk()
+root.geometry("300x200")
+root.resizable(True, True)
+root.title("Slider Demo")
+
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=3)
+
+
+# slider current value
+current_value = tk.IntVar()
+
+
+def get_current_value():
+    return "{}".format(current_value.get())
+
+
+def slider_changed(event):
+    text = get_current_value()
+    value_label.configure(text=bin(int(text)))
+    print(bin(int(text)))
+
+
+# label for the slider
+slider_label = ttk.Label(root, text="Slider:")
+
+slider_label.grid(column=0, row=0, sticky="w")
+
+#  slider
+slider = tk.Scale(root, from_=0, to=1024, tickinterval=1, orient="horizontal", command=slider_changed, variable=current_value)  # vertical
+slider.grid(column=0, row=1, sticky="we", columnspan=10)
+
+# current value label
+current_value_label = ttk.Label(root, text="Current Value:")
+
+current_value_label.grid(row=4, columnspan=2, sticky="n", ipadx=10, ipady=10)
+
+# value label
+value_label = ttk.Label(root, text=get_current_value())
+value_label.grid(row=2, columnspan=2, sticky="n")
+
+
+root.mainloop()

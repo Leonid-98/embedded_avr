ce5aef9d396f4e1a8a32cd205261fa735cb90554
Leonid-98 <leonid.tsigrinski@gmail.com>
2022-05-04 02:21:56 +0300

    123

--- c_praks_3/yl1.c
+++ c_praks_3/yl1.c
@@ -5,8 +5,14 @@
 #include "song2.h"
 
 ISR(TIMER1_COMPA_vect){
+	// prescaler 1024
 	PORTB ^= 1<<PB7;
 	PORTA ^= 0xFF;
+}
+
+uint16_t calculateTimerDelay(uint16_t delayMs) {
+	// https://moodle.ut.ee/pluginfile.php/189029/mod_resource/content/12/ASM%203%20praktikum.pdf
+	return (delayMs - delayMs/32 + delayMs/128)*2048/2000;
 }
 
 int main(void){
@@ -14,7 +20,7 @@ int main(void){
 	TCCR1A = (1<<WGM10) | (0<<WGM11);
 	TCCR1B = (0<<WGM12) | (1<<WGM13) | (1<<CS12) | (0<<CS11) | (1<<CS10);
 	TIMSK1 = 1<<OCIE1A;  // interrupt en
-	OCR1A = 128; // blink delay, 16bit max
+	OCR1A = calculateTimerDelay(500); // blink delay, 16bit max
 	
 	// buzzer init
 	DDRB = 0xFF;
